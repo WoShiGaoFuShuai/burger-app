8d910cf1d22f9549f5ae8fd724a6e530b0cb6dfb
Maksym Yefremov <maksymyefremov@Maksyms-Laptop.local>
2024-07-22 21:34:41 +0800

    added ui folder with tabs render file

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -3,26 +3,24 @@ import cl from "@/components/burger-ingredients/burger-ingredients.module.css";
 import IngredientsGroup from "@/components/burger-ingredients/ingredients-group/ingredients-group";
 import { dataBuns, dataSauce } from "@/utils/data";
 
-import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
+// import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
+import TabsRender from "@/components/ui/tabs/tabs-render";
+import { TabValue } from "@/components/ui/tabs/interfaces.tabs-render";
 
 const BurgerIngredients = () => {
   const [current, setCurrent] = React.useState("one");
+  const tabsValues: TabValue[] = [
+    { value: "one", name: "Булки" },
+    { value: "two", name: "Соусы" },
+    { value: "three", name: "Начинки" },
+  ];
+
   return (
     <section className={cl.wrapper}>
       <p className={`text text_type_main-large ${cl.title}`}>Соберите бургер</p>
 
       {/* TABS */}
-      <div className={cl.tabs} style={{ display: "flex" }}>
-        <Tab value="one" active={current === "one"} onClick={setCurrent}>
-          Булки
-        </Tab>
-        <Tab value="two" active={current === "two"} onClick={setCurrent}>
-          Соусы
-        </Tab>
-        <Tab value="three" active={current === "three"} onClick={setCurrent}>
-          Начинки
-        </Tab>
-      </div>
+      <TabsRender tabs={tabsValues} current={current} setCurrent={setCurrent} />
 
       <div className={cl.ingredients__wrapper}>
         {/* //BUNS  */}
